c3912254b5c64839d573d8db95879e60a51a2cce
Dmitry <ecawd>
2021-12-07 22:10:50 +0300

    v2

--- Target two/ViewController.swift
+++ Target two/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var buttonStart: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.backgroundColor = .red
+        redView.layer.cornerRadius = 50
+        redView.alpha = 0.3
+        yellowView.backgroundColor = .yellow
+        yellowView.layer.cornerRadius = 50
+        yellowView.alpha = 0.3
+        greenView.backgroundColor = .green
+        greenView.layer.cornerRadius = 50
+        greenView.alpha = 0.3
     }
-
+    
+    @IBAction func startingButton(_ sender: UIButton) {
+        buttonStart.setTitle("Next", for: .normal)
+        if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+        } else if yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else if greenView.alpha != 1 && redView.alpha != 1{
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+        
+// не смог понять почему такой вариант не работает :(
+//
+//        if redView.alpha == 0.3 &&
+//            yellowView.alpha == 0.3 &&
+//            greenView.alpha == 0.3 {
+//            redView.alpha = 1
+//        } else if redView.alpha == 1 &&
+//                    yellowView.alpha == 0.3 &&
+//                    greenView.alpha == 0.3{
+//            redView.alpha = 0.3
+//            yellowView.alpha = 1
+//        } else if yellowView.alpha == 1 {
+//            yellowView.alpha = 0.3
+//            greenView.alpha = 1
+//        } else if redView.alpha == 0.3 &&
+//                    yellowView.alpha == 0.3 &&
+//                    greenView.alpha == 1 {
+//            greenView.alpha = 0.3
+//        }
+    }
+    
 
 }
 
